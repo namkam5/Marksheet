94c9d036cb3e79ce93ae4b7b1d580748ca798beb
namkam5 <namankamran@gmail.com>
2016-04-29 15:22:14 +0400

    removed sign in

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 gem 'sinatra'
 ruby "2.3.0"
 gem 'angularjs-rails', '~> 1.5'
+
--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/reloader' if development?
 
 class Main < Sinatra::Base
 
